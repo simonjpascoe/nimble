cb585b9f8694b89e2a68228e6af9844375a43472
Simon Pascoe <simon.j.pascoe@gmail.com>
2020-12-13 23:46:28 +0800

    Day 13. Oops.

--- interactive.fsx
+++ interactive.fsx
@@ -1,13 +1,18 @@
 #load @"./lib/aoc.fs"
-#load @"./aoc2020/D12.fsx"
+#load @"./lib/math.fs"
+#load @"./lib/functional.fs"
+#load @"./aoc2020/D13.fsx"
 
+open Nimble.Math
 open Nimble.AOC
-open D12
+open Nimble.Functional
+open D13
 
-let input = loadInput 2020 12
+let input = loadInput 2020 13
 
 let TIME = true
 let f f = if TIME then timeit f else f()
 
-let output1 = f (fun () -> day12a input)
-let output2 = f (fun () -> day12b input)
+//let output1 = f (fun () -> day13a input)
+let output1 = day13a input
+let output2 = day13b input
--- lib/functional.fs
+++ lib/functional.fs
@@ -3,5 +3,21 @@ module Nimble.Functional
 let first  f (a, b) = (f a, b)
 let second f (a, b) = (a, f b)
 
-let ( &&& ) f a b = (f a, f b)
-let ( *** ) f g a b = (f a, g b)
+let ( &&& ) f g a = (f a, g a)
+let ( *** ) f g (a, b) = (f a, g b)
+
+
+module List =
+  let fold1 fn xs =
+    match xs with
+      | [] -> failwith "Empty list provided, no state to lift"
+      | x::xs -> List.fold fn x xs
+
+  let rec treeReduce (fn: 'a->'a->'a) (xs: 'a list) =
+    match xs with
+      | [] -> failwith "Called with empty list"
+      | [x] -> x
+      | ys -> let ln = List.length ys / 2
+              let ys1, ys2 = (List.take ln &&& List.skip ln) ys
+              let zs1, zs2 = (treeReduce fn ys1, treeReduce fn ys2)
+              fn zs1 zs2
