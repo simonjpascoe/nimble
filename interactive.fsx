067384ee37d458e400bf2ee9527bcc50fa3c4c85
Simon Pascoe <simon.j.pascoe@gmail.com>
2020-12-13 12:26:00 +0800

    D12 interactive

--- interactive.fsx
+++ interactive.fsx
@@ -1,13 +1,13 @@
 #load @"./lib/aoc.fs"
-#load @"./aoc2020/D11.fsx"
+#load @"./aoc2020/D12.fsx"
 
 open Nimble.AOC
-open D11
+open D12
 
-let input = loadInput 2020 11
+let input = loadInput 2020 12
 
 let TIME = true
 let f f = if TIME then timeit f else f()
 
-let output1 = f (fun () -> day11a input)
-let output2 = f (fun () -> day11b input)
+let output1 = f (fun () -> day12a input)
+let output2 = f (fun () -> day12b input)
