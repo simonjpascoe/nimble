143a91f6f7870a1aee94d36b33fefbcb60ba7673
Simon Pascoe <simon.j.pascoe@gmail.com>
2020-12-13 23:49:30 +0800

    D14/15 prep.

---
+++ aoc2020/D14.fsx
@@ -0,0 +1,13 @@
+#load @"./../lib/math.fs"
+#load @"./../lib/functional.fs"
+#load @"./../lib/aoc.fs"
+
+open Nimble.Math
+open Nimble.Functional
+open Nimble.AOC
+
+
+let day14a (input : string list) = input
+
+let day14b (input : string list) = input
+
