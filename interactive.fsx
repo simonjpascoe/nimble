c13401e50dd1ec19e8d9bad38c69dc7a3fdbc735
Simon Pascoe <simon.j.pascoe@gmail.com>
2020-12-17 00:14:55 +0800

    D16, really nice problem and for once, a really neat solution!

--- interactive.fsx
+++ interactive.fsx
@@ -1,18 +1,18 @@
 #load @"./lib/aoc.fs"
 #load @"./lib/math.fs"
 #load @"./lib/functional.fs"
-#load @"./aoc2020/D14.fsx"
+#load @"./aoc2020/D16.fsx"
 
 open Nimble.Math
 open Nimble.AOC
 open Nimble.Functional
-open D14
+open D16
 
-let input = loadInput 2020 14
+let input = loadInput 2020 16
 
 let TIME = true
 let f f = if TIME then timeit f else f()
 
 //let output1 = f (fun () -> day13a input)
-let output1 = day14a input
-let output2 = day14b input
+let output1 = day16a input
+let output2 = day16b input
