8fe58c78a6191530e93c113473de1745fed6db44
Simon Pascoe <simon.j.pascoe@gmail.com>
2020-12-17 00:26:15 +0800

    (shorter)

--- aoc2020/D16.fsx
+++ aoc2020/D16.fsx
@@ -8,37 +8,6 @@ open Nimble.AOC
 
 open System.Text.RegularExpressions
 
-
-let input16A = [
-  "class: 1-3 or 5-7"
-  "row: 6-11 or 33-44"
-  "seat: 13-40 or 45-50"
-  ""
-  "your ticket:"
-  "7,1,14"
-  ""
-  "nearby tickets:"
-  "7,3,47"
-  "40,4,50"
-  "55,2,20"
-  "38,6,12"
-]
-
-let input16B = [
-  "class: 0-1 or 4-19"
-  "row: 0-5 or 8-19"
-  "seat: 0-13 or 16-19"
-  ""
-  "your ticket:"
-  "11,12,13"
-  ""
-  "nearby tickets:"
-  "3,9,18"
-  "15,1,5"
-  "5,14,9"
-]
-
-
 let preprocess (input: string list) =
   let marker1 = input |> List.findIndex(fun x -> x = "your ticket:")
 
