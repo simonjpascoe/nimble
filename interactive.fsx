eb6a96b0bb219efe7326c9fb966915f981750e8a
simonjpascoe <simon.j.pascoe+github@gmail.com>
2021-12-12 13:54:41 +0800

    D14/15 prep.

--- interactive.fsx
+++ interactive.fsx
@@ -1,18 +1,18 @@
 #load @"./lib/aoc.fs"
 #load @"./lib/math.fs"
 #load @"./lib/functional.fs"
-#load @"./aoc2020/D13.fsx"
+#load @"./aoc2020/D14.fsx"
 
 open Nimble.Math
 open Nimble.AOC
 open Nimble.Functional
-open D13
+open D14
 
-let input = loadInput 2020 13
+let input = loadInput 2020 14
 
 let TIME = true
 let f f = if TIME then timeit f else f()
 
 //let output1 = f (fun () -> day13a input)
-let output1 = day13a input
-let output2 = day13b input
+let output1 = day14a input
+let output2 = day14b input
