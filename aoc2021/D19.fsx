50584f6815c30f076aefd11f5e3d23e7502ab683
simonjpascoe <simon.j.pascoe+github@gmail.com>
2022-01-04 00:11:23 +0800

    D19 redux (WIP)

--- aoc2021/D19.fsx
+++ aoc2021/D19.fsx
@@ -8,7 +8,7 @@ open Nimble.Functional
 open Nimble.AOC
 
 type P3 = int * int * int
-type M3c = P3 * P3 * P3
+type M3 = P3 * P3 * P3
 
 let sortIndex xs = xs |> List.mapi (fun i x -> i, x) |> List.sortBy snd |> List.map fst
 
@@ -16,6 +16,15 @@ let zero = (0,0,0)
 
 let d0 n d = if d = 0 then 0 else n / d
 let div (x1, y1, z1) (x2, y2, z2) = (d0 x1 x2, d0 y1 y2, d0 z1 z2)
+
+
+let cr3 (x1,y1,z1) (x2,y2,z2) : M3 =
+  // (assuming no degeneration here)
+  let f a b = if a = b then 1 else if a = -b then -1 else 0
+  let r1 = (f x1 x2, f x1 y2, f x1 z2)
+  let r2 = (f y1 x2, f y1 y2, f y1 z2)
+  let r3 = (f z1 x2, f z1 y2, f z1 z2)
+  (r1,r2,r3)
 
 let d3d (x1, y1, z1) (x2, y2, z2) =
   ((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2)) |> double |> Math.Sqrt
@@ -52,44 +61,31 @@ let analyse (data : Map<int, P3 list>) =
               if s1 < s2 then
                 let xs1 = a1.[s1]
                 let xs2 = a1.[s2]
-                yield xs1 |> List.choose (fun t1 -> let ys = xs2 |> List.filter (fun t2 -> fth4 t1 = fth4 t2)
-                                                    if List.isEmpty ys then None else Some (s1, s2, t1, ys))]
+                yield xs1 |> List.collect (fun (x10,x11,x12,p1) -> 
+                                xs2 |> List.choose (fun (x20,x21,x22,p2) -> 
+                                    if p1 = p2 then Some (s1, s2, (x10,x11,x12), (x20,x21,x22)) else None))]
            |> List.concat
-           |> List.groupBy (fun (s1, s2, p1, ps) -> (s1, s2))
-           |> List.map (second (List.map (fun x -> (thd4 x, fth4 x))))
+          //  |> List.groupBy (fun (s1, s2, p1, ps) -> (s1, s2))
+          //  |> List.map (second (List.map (fun (_,_,c,d) -> (c,d))))
 
-  a2
-  // // find rotations and translations that line up the triangles identified
-  // // the detection is necessary but not sufficient ot identify a transform
-  // // the resulting vectors below need to be equal and basis vectors to signal a similarity
-  // // (even then, technically, it's not quite sufficient...)
-  // // test with head
-  // let align ((s1, s2), xs) =
-  //   let align1 ((p1, p2, p3, _), ys) =
-  //     let pair2 xs y = xs |> List.find (fun y -> (d3d zero y) = (d3d zero y))
-  //     let pair3 xs y =
-  //       let z = xs |> List.find (fun y -> (d3d zero y) = (d3d zero y))
-  //       let y2 = y |> List.map abs |> sortIndex
-  //       let z2 = z |> List.map abs |> sortIndex
+  // now need to find the relevant rotation matrix for each triangle. because the coordinate systems
+  // are inconsistent, we can align the vectors from points to points before finding the matrix that aligns these
+  // as in absolute space, the vectors from a -> b -> c -> a all are the same
+  // expand an example
+  let align1 a3 =
+    let (s1, s2, (p1,p2,p3), (q1,q2,q3)) = a3
 
+    let vgen s ps = ps @ [List.head ps] |> List.pairwise |> List.map (fun (p, q) -> vec data.[s].[p] data.[s].[q]) |> debugn
+      
+    let pvs = vgen s1 [p1;p2;p3]
+    let uvs = vgen s2 [q1;q2;q3]
+    let pair p = uvs |> List.find (fun u -> d3d zero u = d3d zero p)
+    let m3s = pvs |> List.map (fun p -> cr3 p (pair p))
+    // if all the same, we accept this possibility...
+    let test = m3s.[1..] |> List.forall (fun x -> x = m3s.[0])
+    if test then Some (s1, s2, m3s.[0]) else None
 
-  //     match ys with
-  //       | [(q1, q2, q3, _)] -> let v1 = vec data.[s1].[p1] data.[s1].[p2]
-  //                              let v2 = vec data.[s1].[p2] data.[s1].[p3]
-  //                              let v3 = vec data.[s1].[p3] data.[s1].[p1]
-  //                              let u1 = vec data.[s2].[q1] data.[s2].[q2]
-  //                              let u2 = vec data.[s2].[q2] data.[s2].[q3]
-  //                              let u3 = vec data.[s2].[q3] data.[s2].[q1]
-  //                              let vs = [v1;v2;v3]
-  //                              let us = [u1;u2;u3]
-  //                              let vs1 = vs |> List.map (pair2 us)
-  //                              let xform = List.map2 div vs vs1
-  //                              let mag = d3d zero xform.[0] = Math.Sqrt(3.0)
-  //                              let equal = xform |> List.forall (fun x -> x = xform.[0])
-  //                              if mag && equal then Choice1Of2 xform.[0] else Choice2Of2 (xform, mag, us, vs)
-  //       | _ -> failwith "illogical 1"
-  //   xs |> List.map (fun x -> x, align1 x)
-  // a2 |> List.map (fun a -> fst a, align a)
+  a2 |> List.choose align1
 
 
 let day19a s =
