3837fc4ed6343a8f115322f9900ba867b61cc869
simonjpascoe <simon.j.pascoe+github@gmail.com>
2022-05-10 23:06:36 +0800

    Move some data for prob 67

--- euler/67.fsx
+++ euler/67.fsx
@@ -1,6 +1,6 @@
-#load "18.fsx"
+#load "./18.fsx"
 open System.IO
 
 let data67 =
-  let triangle = File.ReadAllLines @"./euler/inputs/p067_triangle.txt"
+  let triangle = File.ReadAllLines @"data/euler/p067_triangle.txt"
   triangle |> Array.map (fun (xs: string) -> xs.Split(' ') |> Array.map int |> Array.toList) |> Array.toList
